3a6a396b6e4d44acc01ba0b45b030b0be08ddb7e
Gleb Kostrov <anegada1@mail.ru>
2022-05-25 11:05:35 +0300

    Added 3D and 2D folders for 2 projects

---
+++ 3D-Graph/Frontend/src/components/app/App.js
@@ -0,0 +1,15 @@
+// // import './App.css';
+
+// import MainPage from '../Page/MainPage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <MainPage></MainPage>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
