ca35b4a703241036e634d22a232de3dc339b702e
Gleb Kostrov <anegada1@mail.ru>
2022-05-25 12:24:12 +0300

    added static property to graph

--- 3D-Graph-Front/public/script.js
+++ 3D-Graph-Front/public/script.js
@@ -7,14 +7,24 @@ fetch(fileName).then(res => res.json()).then(data => {
         <ForceGraph3D
         graphData={data}
         nodeAutoColorBy="group"
+        linkWidth = {1}
+        linkCurvature = {0.2}
+        linkOpacity = {0.5}
+        onNodeRightClick={(node) => {
+            window.open(node.site);
+          }}
+          onNodeDragEnd={node => {
+            node.fx = node.x;
+            node.fy = node.y;
+            node.fz = node.z;
+          }}
         nodeThreeObject={node => {
-
             const sprite = new SpriteText(node.description);
             sprite.color = node.color;
-            sprite.textHeight = 8;
+            sprite.textHeight = 6;
             if (node.id > 200) {
-            sprite.textHeight = 20;
-            }
+            sprite.textHeight = 18;
+        }
             
             return sprite;
         }}
